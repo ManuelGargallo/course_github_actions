4c895a4e99e2f8fc89913a7faaaa47fbd09aaf6f
manu <manuelperezgargallo@gmail.com>
2026-05-23 19:22:19 +0100

    Add new functions

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,5 +1,9 @@
-from app.calculations import add_numbers
+from app.calculations import add_numbers, diff_numbers
 
 
 def test_add_numbers_correct():
     assert add_numbers(10, 5) == 15
+
+
+def test_diff_numbers_correct():
+    assert diff_numbers(10, 5) == 5
